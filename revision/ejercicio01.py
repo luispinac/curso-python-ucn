07776f1ca57d72eb4bfec5d867665febe4a557c7
Luis Piña Cabezas <luis.pina.cabezas@sqm.com>
2020-04-14 10:47:53 -0400

    commit inicial

---
+++ revision/ejercicio01.py
@@ -0,0 +1,12 @@
+def prueba1(num1, num2):
+    return num1 + num2
+
+# class q1():
+#     def __init__(self):
+#         print("Hola, te estoy saludando desde el __init__"  \
+#               "de la clase Saludo")
+    
+#     def imprimirPrueba1():
+#         print("esta es una prueba")
+
+q2 = 41
