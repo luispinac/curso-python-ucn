26d80d785b68f910472df277400aa63b4a428898
Luis Piña Cabezas <luis.pina.cabezas@sqm.com>
2020-04-20 01:37:51 -0400

    agregada conexion

---
+++ revision/conexion.py
@@ -0,0 +1,15 @@
+def conectar():
+    pip install requests
+    pip install -r requirements.txt
+    from firebase import firebase
+    print("Todo ok")
+    return
+
+# class q1():
+#     def __init__(self):
+#         print("Hola, te estoy saludando desde el __init__"  \
+#               "de la clase Saludo")
+    
+#     def imprimirPrueba1():
+#         print("esta es una prueba")
+
